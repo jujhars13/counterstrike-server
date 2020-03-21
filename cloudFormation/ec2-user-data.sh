c50a8b6db901ef992721097b00ebc099b7c73a86
Jujhar Singh <jujhar@jujhar.com>
2020-03-21 19:17:26 +0000

    ready to run

--- cloudFormation/ec2-user-data.sh
+++ cloudFormation/ec2-user-data.sh
@@ -4,14 +4,17 @@ readonly logName="/var/log/server-setup.log"
 
 echo "Starting " | tee -a "${logName}"
 
-
 echo "Install required tools " | tee -a "${logName}" 
 yum install -y \
   docker \
   iptraf-ng \
   htop \
   tmux \
-  vim
+  vim \
+  curl
+
+echo "Setting up ssh access"  | tee -a "${logName}"
+curl -s https://github.com/jujhars13.keys | tee -a /home/ec2-user/.ssh/authorized_keys
 
 # add ec2 user to the docker group which allows docket to run without being a super-user
 usermod -aG docker ec2-user
@@ -25,7 +28,7 @@ sleep 2s
 
 echo "Creating rudemntary web app " | tee -a "${logName}"
 mkdir -p /home/ec2-user/webapp
-echo "<html><body><h1>Welcome CS peeps</h1><div> We hope you enjoy our webpage. - The Economist</div></body></html>" | tee -a /home/ec2-user/webapp/index.html
+echo "<html><body><h1>Welcome CS peeps</h1><div> We hope you enjoy our cs server</div></body></html>" | tee -a /home/ec2-user/webapp/index.html
 
 echo "Starting nginx container " | tee -a "${logName}"
 docker run -d \
--- run.sh
+++ run.sh
@@ -3,20 +3,38 @@
 # ensure AWS_PROFILE/SECRET_ACCESS are set before
 
 readonly FILE_DIRECTORY=$(dirname "${BASH_SOURCE[0]}")
-readonly TEMPLATE="${FILE_DIRECTORY}/cloudFormation/01-network.yml"
 readonly STACK_NAME="cs-server"
 readonly BATCH="batch-${RANDOM}"
 readonly NOW=$(date +%Y-%m-%dT%H:%M:%S)
-readonly TAGS=("ApplicationName=cs-server" "Batch=${BATCH}" "DateCreation=${NOW}" "StackName=${STACK_NAME}" "Owner=Jujhar@jujhar.com")
+readonly TAGS=("ApplicationName=cs-server" "Batch=${BATCH}" "DateCreation=${NOW}" "StackName=${STACK_NAME}" "Owner=jujhar@jujhar.com")
 readonly AWS_DEFAULT_REGION="eu-west-1"
+readonly UserDataScript="$(< ${FILE_DIRECTORY}/cloudFormation/ec2-user-data)"
 
-# This prevents an issue with the expansion of the variable $PARAMETERS
-# shellcheck disable=SC2086
+# deploy network
 aws cloudformation deploy \
-  --template-file "${TEMPLATE}" \
+  --template-file "${FILE_DIRECTORY}/cloudFormation/01-network.yml" \
   --stack-name "${STACK_NAME}" \
   --capabilities CAPABILITY_IAM \
   --tags "${TAGS[@]}" \
   --no-fail-on-empty-changeset \
-  --parameter-overrides ${PARAMETERS} \
+  --region "${AWS_DEFAULT_REGION}"
+
+echo "Finding the current Amazon Linuz 2 AMI"
+readonly LINUX2_AMI=$(aws ec2 describe-images \
+  --owners amazon \
+  --filters 'Name=name,Values=amzn2-ami-hvm-2.0.????????.?-x86_64-gp2' 'Name=state,Values=available' \
+  --query 'reverse(sort_by(Images, &CreationDate))[:1].ImageId' \
+  --output text)
+echo "This is the current Linux 2 AMI: ${LINUX2_AMI}"
+
+# deploys server
+aws cloudformation deploy \
+  --template-file "${FILE_DIRECTORY}/cloudFormation/02-server.yml" \
+  --stack-name "${STACK_NAME}" \
+  --capabilities CAPABILITY_IAM \
+  --tags "${TAGS[@]}" \
+  --no-fail-on-empty-changeset \
+  --parameter-overrides \
+  Linux2AMI="${LINUX2_AMI}" \
+  UserDataScript="${UserDataScript}" \
   --region "${AWS_DEFAULT_REGION}"
